b3f7e14eef18d7697ae4985b8559e637ae10d7b6
michalbaranowski <michal_baranowski@o2.pl>
2023-01-22 12:40:12 +0100

    try catch for unexpected exceptions

--- EasyCI/Program.cs
+++ EasyCI/Program.cs
@@ -12,30 +12,48 @@ var gitRunner = new GitCommandsRunner(appPathResolver, envConfigProvider);
 var dockerRunner = new DockerCommandsRunner(appPathResolver, envConfigProvider);
 
 string prevCommitSha = string.Empty;
+bool shouldStop = false;
+int exceptionsCounter = 0;
 
 do
 {
-    var lastCommitSha = repoStatusProvider.GetLastCommitSha();
-    if (prevCommitSha != lastCommitSha)
+    try
     {
-        prevCommitSha = lastCommitSha;
+        var lastCommitSha = repoStatusProvider.GetLastCommitSha();
+        if (prevCommitSha != lastCommitSha)
+        {
+            prevCommitSha = lastCommitSha;
 
-        gitRunner.Pull();
+            gitRunner.Pull();
 
-        var dictionary = dockerRunner.GetDockerContainerIdsWithImageIds();
-        var containerIdList = dictionary.Select(n => n.Key).ToList();
-        var imageIdList = dictionary.Select(n => n.Value).Distinct().ToList();
+            var dictionary = dockerRunner.GetDockerContainerIdsWithImageIds();
+            var containerIdList = dictionary.Select(n => n.Key).ToList();
+            var imageIdList = dictionary.Select(n => n.Value).Distinct().ToList();
 
-        dockerRunner.Build();
-        dockerRunner.StopContainers(containerIdList);
-        dockerRunner.RemoveContainers(containerIdList);
-        dockerRunner.RemoveImages(imageIdList);
-        dockerRunner.Run();
-    } 
-    else
-    {
-        Console.WriteLine("No new commits!");
+            dockerRunner.Build();
+            dockerRunner.StopContainers(containerIdList);
+            dockerRunner.RemoveContainers(containerIdList);
+            dockerRunner.RemoveImages(imageIdList);
+            dockerRunner.Run();
+        }
+        else
+        {
+            var now = DateTime.UtcNow;
+            Console.WriteLine($"{now}: No new commits!");
+        }
+
+        Thread.Sleep(TimeSpan.FromSeconds(10));
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
 
-    Thread.Sleep(10000);
-} while (true);
+        exceptionsCounter++;
+        if (exceptionsCounter == 10)
+        {
+            break;
+        }
+
+        Thread.Sleep(TimeSpan.FromMinutes(1));
+    }
+} while (shouldStop == false);
